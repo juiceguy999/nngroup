cb6174c0f6703974a9fab9059224494e5ab988d6
Nurlan <hotabyc8@gmail.com>
2024-07-19 12:21:10 +0400

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+// import { onMounted, ref } from 'vue';
+
+
+// const myData = ref(null);
+
+// onMounted(async () => {
+//   const res = await fetch('/foods');
+//   const data = await res.json();
+//   myData.value = data;
+// })
+</script>
+
+<template>
+  <main>
+    Hello world
+  </main>
+</template>
