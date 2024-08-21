670ff8c312027346ec9c78ef454912e1934b3aff
Nurlan <hotabyc8@gmail.com>
2024-08-21 16:57:18 +0400

    Register test

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,27 +1,50 @@
+<script setup>
+import { ref } from 'vue';
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const fields = ref({
+  name: "",
+  email: "",
+  password: "",
+  password_confirmation: ""
+})
+
+async function register(payload) {
+  const res = await fetch(`${apiUrl}/register`, {
+    method: 'POST',
+    body: JSON.stringify(payload)
+  });
+
+  console.log(res);
+}
+</script>
+
 <template>
   <main class="main">
     <section class="register">
       <div class="container">
         <h1 class="register__title">Login to NNGROUP</h1>
         <div class="register__inner">
-          <form class="register__form">
+          <form @submit.prevent="register(fields)" class="register__form">
             <p class="register__form-text">Please fill out this fields</p>
             <div class="register__form-group">
               <label for="name">Name</label>
-              <input id="name" placeholder="Name" type="text">
+              <input v-model="fields.name" id="name" placeholder="Name" type="text">
             </div>
             <div class="register__form-group">
               <label for="email">Email</label>
-              <input id="email" placeholder="Email" type="text">
+              <input v-model="fields.email" id="email" placeholder="Email" type="text">
             </div>
             <div class="register__form-group">
               <label for="password">Password</label>
-              <input id="password" placeholder="Password" type="text">
+              <input v-model="fields.password" id="password" placeholder="Password" type="text">
             </div>
             <div class="register__form-group">
               <label for="password-confirm">Confirm password</label>
-              <input id="password-confirm" placeholder="Confirm password" type="text">
+              <input v-model="fields.password_confirmation" id="password-confirm" placeholder="Confirm password" type="text">
             </div>
+            <button class="register__form-btn" type="submit">Submit</button>
           </form>
         </div>
       </div>
@@ -32,7 +55,7 @@
 <style scoped>
 
   .register {
-    padding-top: 50px;
+    padding: 50px 0;
   }
 
   .register__title {
@@ -66,6 +89,22 @@
     border-radius: 6px;
     padding: 0 10px;
     border: 1px solid #737373;
+  }
+
+  .register__form-btn {
+    padding: 10px 15px;
+    border-radius: 6px;
+    border: none;
+    background: #000;
+    color: #fff;
+    font-family: 'Inter', sans-serif;
+    margin: 0 auto;
+    margin-top: 40px;
+    font-weight: 600;
+    cursor: pointer;
+    width: 100%;
+    max-width: 184px;
+    display: block;
   }
 
 </style>
